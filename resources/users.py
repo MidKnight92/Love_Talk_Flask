411a146145713c0f69f4cf878294512b1a8b7bc2
Stephanie Viveros <sviveros05@gmail.com>
2019-11-19 21:21:55 -0600

    Created Users Routes

--- resources/users.py
+++ resources/users.py
@@ -8,6 +8,25 @@ from playhouse.shortcuts import model_to_dict
 users = Blueprint('users', 'users')
 
 @users.route('/', methods=['GET'])
-def test_user_controller():
+def list_users():
 	return "heeyyyyyyyyyy"
-	
+
+# POST(ing data) user is registering
+@users.route('/register', methods=['POST'])
+def register():
+	# payload = request.get_json()
+	return "register"
+
+# POST(ing data) USER IS LOGGING IN
+@users.route('/login', methods=['POST'])
+def login():
+	return 'login'
+
+# EDIT USERS PROFILE
+@users.route('/', methods=['PUT'])
+def update_user():
+	return 'EDIT'
+
+@users.route('/logout', methods=['GET'])
+def logout():
+	return 'logout'
